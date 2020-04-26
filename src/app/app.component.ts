e285c483280bab3599f95b63ff7697c559b209d9
Josue <josue.rodriguez1551@gmail.com>
2020-04-25 22:03:53 -0600

    Uploading project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+  title:string = 'todolist';
+  name:string = 'Josue';
+  
+  constructor(){
+    this.name = 'David!';
+  }
+
+  changeName(name:string):void {
+    this.name = name;
+  }
 }
